a051488bd0a9838c3b02d698f6a19633286332dd
laxbuddy1 <jcyachimowicz131@gmail.com>
2018-06-19 19:49:55 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,15 +1,13 @@
 def fizz_buzz(int)
   if int%15 ==0
-    puts "fizz"
-  end
-elsif int%3 ==0
-    puts "buzz"
-  end
-elsif int%5 ==0
     puts "fizzbuzz"
-  end
-else "nil"
+elsif int%3 ==0
+    puts "fizz"
+elsif int%5 ==0
+    puts "buzz"
+else
+  puts "nil"
   end
 end
 
-fizz_buzz(15)
+fizz_buzz(30)
